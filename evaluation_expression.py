0592ccce4c1a1fd5223b673f32334792d6b1dd8a
Florent <39738292+FlorentRu@users.noreply.github.com>
2019-05-13 21:59:03 -0400

    Priority rules

---
+++ evaluation_expression.py
@@ -0,0 +1,16 @@
+'''
+Created on Jul 25, 2017
+
+@author: SummitWorks
+'''
+
+# a,b,c,d,e,f=10,4,2,1,10,2
+# x = a+b/c-e/f+d;
+# print(x)
+a,b,c,d,e,f=10,4,2,3,10,2
+x = (a+b)/c-e/(f+d);
+print(x)
+# x/c-e/y
+
+print(a<b)
+print(a>b)
