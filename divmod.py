ead551a40ceb2c90d9eaa00fd94c34aab82823db
Florent Rudel <39738292+FlorentRu@users.noreply.github.com>
2019-05-23 03:08:14 -0400

    Update divmod.py

--- divmod.py
+++ divmod.py
@@ -1,7 +1,6 @@
 '''
-Created on Jun 15, 2017
+Created on Jun 15, 2018
 
-@author: SummitWorks
 '''
 days = int(input("Enter days: "))
 #The divmod(num1, num2) function returns two values , first is the division of num1 and num2 and in second the modulo of num1 and num2.
