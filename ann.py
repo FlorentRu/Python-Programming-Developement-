819246a3199579eb96970fc9ab2cfde38d0833b7
Florent Rudel <39738292+FlorentRu@users.noreply.github.com>
2019-06-06 20:34:32 -0400

    Update ann.py

--- ann.py
+++ ann.py
@@ -1,13 +1,10 @@
-# Artificial Neural Network
+# Making a predictive model using the Artificial Intelligence to predict the efficacy of the network once it is deployed
+# I am expecting a dataset of cathegorical variables: 
+# Independent variables (Distance between civilians and ducks, or the weather, I have seen in one video that humidity matters too, ...)
+# Dependant Variables (Map: {0: people will be able to use the duck's network , 1: people won't be able to use the duck's network })
+# Programming language used: Python
 
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
-
-# Installing Keras
-# pip install --upgrade keras
+# Beginning of the Artificial Neural Network algorithm
 
 # Part 1 - Data Preprocessing
 
@@ -16,10 +13,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
-dataset = pd.read_csv('Churn_Modelling.csv')
-X = dataset.iloc[:, 3:13].values
-y = dataset.iloc[:, 13].values
+# Importing the dataset of categorical variable
+dataset = pd.read_csv('CathegoricalData.csv')
+X = dataset.iloc[:, 5:20].values # Independent Variables what I will need from you under a csv file if possible
+y = dataset.iloc[:, 20].values # Dependent Variables what I will need from you under a csv file if possible
 
 # Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
@@ -41,7 +38,7 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Part 2 - Now let's make the ANN!
+# Part 2 - making the ANN
 
 # Importing the Keras libraries and packages
 import keras
@@ -52,7 +49,7 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Adding the input layer and the first hidden layer
-classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11))
+classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11)) # Activation function
 
 # Adding the second hidden layer
 classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
